edae1cbdf25bd8e9af37f200468770232289099f
Dmitry Safronov <qwinmen@ya.ru>
2018-01-24 18:45:50 +0300

    MS-1 Open Word file

--- Monolit/Monolit/Form1.Designer.cs
+++ Monolit/Monolit/Form1.Designer.cs
@@ -28,20 +28,85 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.statusBottomBar = new System.Windows.Forms.StatusStrip();
+			this.menuTopBar = new System.Windows.Forms.MenuStrip();
+			this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripStatusLabel_OpenedFileName = new System.Windows.Forms.ToolStripStatusLabel();
+			this.statusBottomBar.SuspendLayout();
+			this.menuTopBar.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// statusBottomBar
+			// 
+			this.statusBottomBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel_OpenedFileName});
+			this.statusBottomBar.Location = new System.Drawing.Point(0, 366);
+			this.statusBottomBar.Name = "statusBottomBar";
+			this.statusBottomBar.Size = new System.Drawing.Size(535, 22);
+			this.statusBottomBar.TabIndex = 0;
+			this.statusBottomBar.Text = "statusStrip1";
+			// 
+			// menuTopBar
+			// 
+			this.menuTopBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+			this.menuTopBar.Location = new System.Drawing.Point(0, 0);
+			this.menuTopBar.Name = "menuTopBar";
+			this.menuTopBar.Size = new System.Drawing.Size(535, 24);
+			this.menuTopBar.TabIndex = 1;
+			this.menuTopBar.Text = "menuStrip1";
+			// 
+			// файлToolStripMenuItem
+			// 
+			this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem});
+			this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+			this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+			this.файлToolStripMenuItem.Text = "Файл";
+			// 
+			// открытьToolStripMenuItem
+			// 
+			this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+			this.открытьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.открытьToolStripMenuItem.Text = "Открыть";
+			this.открытьToolStripMenuItem.ToolTipText = "Загрузить docx файл";
+			this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+			// 
+			// toolStripStatusLabel_OpenedFileName
+			// 
+			this.toolStripStatusLabel_OpenedFileName.Name = "toolStripStatusLabel_OpenedFileName";
+			this.toolStripStatusLabel_OpenedFileName.Size = new System.Drawing.Size(52, 17);
+			this.toolStripStatusLabel_OpenedFileName.Text = "Файл: --";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(411, 175);
+			this.ClientSize = new System.Drawing.Size(535, 388);
+			this.Controls.Add(this.statusBottomBar);
+			this.Controls.Add(this.menuTopBar);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MainMenuStrip = this.menuTopBar;
 			this.Name = "Form1";
 			this.Text = "Monolit Soft";
+			this.statusBottomBar.ResumeLayout(false);
+			this.statusBottomBar.PerformLayout();
+			this.menuTopBar.ResumeLayout(false);
+			this.menuTopBar.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.StatusStrip statusBottomBar;
+		private System.Windows.Forms.MenuStrip menuTopBar;
+		private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_OpenedFileName;
 	}
 }
 
--- Monolit/Monolit/Form1.cs
+++ Monolit/Monolit/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using word = Microsoft.Office.Interop.Word;
 
 namespace Monolit
 {
@@ -17,7 +18,31 @@ namespace Monolit
 			InitializeComponent();
 		}
 
+		private word.Document _document;
 
+		private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			var openFileDialog = new OpenFileDialog
+			{
+				Filter = @"Файлы word|*.docx;*.doc"
+			};
 
+			if (openFileDialog.ShowDialog() != DialogResult.OK)
+				return;
+			word.Application app = new word.Application();
+			try
+			{
+				_document = app.Documents.Open(openFileDialog.FileName);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(@"Ошибка при открытии документа. " + ex.Message);
+				toolStripStatusLabel_OpenedFileName.Text = @"Файл: -error-";
+				return;
+			}
+			toolStripStatusLabel_OpenedFileName.Text = @"Файл: " + _document.Name;
+			_document.Close();
+			app.Quit();
+		}
 	}
 }
